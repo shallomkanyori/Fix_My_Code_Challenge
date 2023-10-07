84b23de8e59f91066fce240601f7120dd95fbf33
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-07 15:15:58 +0000

    Use long name for area function

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class Square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area(self):
+    def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
@@ -30,5 +30,5 @@ if __name__ == "__main__":
 
     s = Square(width=12, height=9)
     print(s)
-    print(s.area())
+    print(s.area_of_my_square())
     print(s.perimiter())
