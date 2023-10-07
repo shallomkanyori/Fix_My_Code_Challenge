b39f33d62be1de2a6c87167e0eddcce88e2df1c8
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-07 15:21:14 +0000

    Use long name for perimeter function

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,7 +6,6 @@ class Square():
     """ Represents a square """
 
     width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """Initialize a square"""
@@ -18,7 +17,7 @@ class Square():
         """ Area of the square """
         return self.width * self.width
 
-    def perimiter(self):
+    def perimiter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 4)
 
@@ -32,4 +31,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimiter())
+    print(s.perimiter_of_my_square())
